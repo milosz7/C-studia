d953ec090c20c0f05739ac8e5bf4308a08c0232b
Milosz B <miloszbargiel@gmail.com>
2022-11-14 18:39:23 +0100

    NWD final version

---
+++ nwd.c
@@ -0,0 +1,110 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_INPUT_LENGTH 21
+
+void clear()
+{
+  while ((getchar()) != '\n')
+    ;
+}
+
+int reset()
+{
+  char input;
+  do
+  {
+    printf("Czy chcesz spróbować jeszcze raz? y/n\n");
+    scanf(" %1c", &input);
+    if (input == 'y')
+    {
+      clear();
+      break;
+    }
+    if (input == 'n')
+      exit(0);
+    clear();
+    printf("Nieprawidłowa operacja!\n");
+  } while (1);
+}
+
+long double input_to_double(char *input)
+{
+  int length = 1;
+  long long int desired_number = 0;
+  for (; input[length]; length++)
+    ;
+  int converted[length];
+  for (int i = 0; i < length; i++)
+  {
+    char sign;
+    sign = input[i];
+    if (sign == '\n')
+      break;
+    if ((int)sign - 48 > 9 || (int)sign - 48 < 0)
+      return NAN;
+    converted[i] = (int)sign - 48;
+  }
+  for (int j = 0; j < length - 1; j++)
+  {
+    desired_number += (long long int)(converted[j] * pow(10, (length - 2 - j)));
+  }
+  return desired_number;
+}
+
+long long get_input(char param_name)
+{
+  char user_input[MAX_INPUT_LENGTH];
+  char clear_buffer[2];
+  long double output = 0;
+  int overflow = 0;
+  printf("Podaj liczbę %c do wyznaczenia NWD:\n", param_name);
+  fgets(user_input, MAX_INPUT_LENGTH, stdin);
+  output = input_to_double(user_input);
+  while (strchr(user_input, '\n') == NULL && clear_buffer[0] != '\n')
+  {
+    fgets(clear_buffer, 2, stdin);
+    overflow = 1;
+  }
+  clear_buffer[0] = 0;
+  if (overflow || isnan(output))
+  {
+    printf("Nieprawidłowy argument!\n");
+    get_input(param_name);
+  }
+  else
+  {
+    return (long long)output;
+  }
+}
+
+void print_result(long long GCF, long long m, long long n)
+{
+  printf("NWD %Ld i %Ld wynosi: %Ld\n", m, n, GCF);
+}
+
+int main()
+{
+  do
+  {
+    long long m, n;
+    long long bigger, smaller;
+    long long modulo;
+
+    m = get_input('m');
+    n = get_input('n');
+
+    bigger = m > n ? m : n;
+    smaller = m > n ? n : m;
+    while (smaller != 0)
+    {
+      modulo = bigger % smaller;
+      bigger = smaller;
+      smaller = modulo;
+    }
+    print_result(bigger, m, n);
+    reset();
+  } while (1);
+}
