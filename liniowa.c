feb411f622665551eef463f78318989fbcd81a6e
Milosz B <miloszbargiel@gmail.com>
2022-12-30 20:46:40 +0100

    Fix a bug with negative numbers

--- liniowa.c
+++ liniowa.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <errno.h>
+#include <string.h>
+#include <regex.h>
 
 #define DOUBLE_INPUT_CHAR_LIMIT 20
+#define NUMBERS_REGEXR "^(-?[0-9]+)?\\.?([0-9]+)?\n?$"
 
 void clear()
 {
   while ((getchar()) != '\n')
     ;
+}
+
+void clear_buffer(char *input)
+{
+  char clear_buffer[2];
+  while (strchr(input, '\n') == NULL && clear_buffer[0] != '\n')
+    fgets(clear_buffer, 2, stdin);
 }
 
 int reset()
@@ -25,6 +36,7 @@ int reset()
     }
     if (input == 'n')
       exit(0);
+    clear();
     printf("Nieprawidłowa operacja!\n");
   } while (1);
 }
@@ -34,18 +46,27 @@ double get_input(char param_symbol)
   double output;
   char *e;
   char user_input[DOUBLE_INPUT_CHAR_LIMIT];
+  regex_t regexr;
+  int is_valid;
+
   printf("Podaj parametr %c funkcji f(x) = ax + b:\n", param_symbol);
   fgets(user_input, DOUBLE_INPUT_CHAR_LIMIT, stdin);
+
+  clear_buffer(user_input);
+
+  is_valid = regcomp(&regexr, NUMBERS_REGEXR, REG_EXTENDED);
+  is_valid = regexec(&regexr, user_input, 0, NULL, 0);
+
   output = strtod(user_input, &e);
-  if (isdigit(*user_input) == 0)
+  if (is_valid == REG_NOMATCH)
   {
     printf("Niepoprawna wartość argumentu %c.\n", param_symbol);
     return get_input(param_symbol);
   }
-  if (!isspace(*e))
+  if (errno == ERANGE)
   {
+    errno = 0;
     printf("Nieprawidłowy argument (konwersja do double jest niemożliwa)!\n");
-    clear();
     return get_input(param_symbol);
   }
   return output;
