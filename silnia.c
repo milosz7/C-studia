33e6111c8152b7c832b613c7538c195de92a343c
Milosz B <miloszbargiel@gmail.com>
2023-01-03 16:58:12 +0100

    Improve silnia to work with any number

--- silnia.c
+++ silnia.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
+#include <regex.h>
+#include <errno.h>
 
-#define MAX_LENGTH 1000
 #define SYSTEM_BASE 10
-#define MAX_FACTORIAL_LENGTH 3
+#define INITIAL_CHUNK 32
+#define NUMBERS_REGEXR "^[1-9][0-9]*?\n$"
+#define ARR_LENGTH(arr_size) (arr_size / sizeof(int))
 
 void clear()
 {
-  while ((getchar()) != '\n');
+  while ((getchar()) != '\n')
+    ;
+}
+
+void clear_buffer(char *input)
+{
+  char clear_buffer[2];
+  while (strchr(input, '\n') == NULL && clear_buffer[0] != '\n')
+    fgets(clear_buffer, 2, stdin);
 }
 
 int reset()
@@ -23,7 +35,8 @@ int reset()
       clear();
       break;
     }
-    if (input == 'n') {
+    if (input == 'n')
+    {
       exit(0);
     }
     clear();
@@ -31,52 +44,91 @@ int reset()
   } while (1);
 }
 
+int get_input()
+{
+  double output;
+  char *e;
+  size_t base_input_size = INITIAL_CHUNK;
+  char *user_input = (char *)malloc(INITIAL_CHUNK);
+  regex_t regexr;
+  int is_valid;
+
+  printf("Podaj liczbę z jakiej chcesz obliczyć silnię:\n");
+  getline(&user_input, &base_input_size, stdin);
+
+  clear_buffer(user_input);
+
+  is_valid = regcomp(&regexr, NUMBERS_REGEXR, REG_EXTENDED);
+  is_valid = regexec(&regexr, user_input, 0, NULL, 0);
+
+  output = strtod(user_input, &e);
+  if (is_valid == REG_NOMATCH)
+  {
+    printf("Podany argument jest niepoprawny!\n");
+    return get_input(user_input);
+  }
+  if (errno == ERANGE)
+  {
+    errno = 0;
+    printf("Podany argument wykracza poza zasięg liczbowy typu double!\n");
+    return get_input(user_input);
+  }
+  return (int)output;
+}
+
+int mem_err()
+{
+  printf("Błąd alokacji pamięci.\n");
+  exit(1);
+}
+
 int main()
 {
   do
   {
-    char input[MAX_FACTORIAL_LENGTH];
-    char result[MAX_LENGTH];
-    int result_reverse[MAX_LENGTH] = {1};
-    int digit, remainder, factorial, last_index, num_start;
-    printf("Podaj liczbę z jakiej chcesz obliczyć silnię:\n");
-    scanf("%3s", input);
-    factorial = atoi(input);
-    if (factorial == 0)
-    {
-      printf("Podany argument nie jest poprawny.\n\n");
-      continue;
-    }
+    int digit, remainder = 0, factorial, current_idx;
+    factorial = get_input();
+    int chunks_to_alloc = 1;
+    int arr_size = sizeof(int) * chunks_to_alloc * INITIAL_CHUNK;
+    int *result_reverse = (int *)calloc(sizeof(int), chunks_to_alloc * INITIAL_CHUNK);
+
+    if (result_reverse == NULL)
+      mem_err();
+    result_reverse[0] = 1;
+
     for (int i = 1; i < factorial + 1; i++)
     {
-      for (int j = 0; j < MAX_LENGTH; j++)
+      for (int j = 0; j < ARR_LENGTH(arr_size); j++)
       {
         digit = result_reverse[j] * i + remainder;
         result_reverse[j] = digit % SYSTEM_BASE;
         remainder = digit / SYSTEM_BASE;
+        for (int k = ARR_LENGTH(arr_size) - 1; k >= 0; k--)
+        {
+          if (result_reverse[k] != 0)
+          {
+            current_idx = k;
+            break;
+          }
+        }
+        if (current_idx == ARR_LENGTH(arr_size) - (remainder % 10) - 1)
+        {
+          chunks_to_alloc += 1;
+          arr_size = arr_size + sizeof(int) * (remainder % 10);
+          result_reverse = (int *)realloc(result_reverse, arr_size);
+          if (result_reverse == NULL)
+            mem_err();
+          for (int i = current_idx + 1; i < ARR_LENGTH(arr_size); i++)
+            result_reverse[i] = 0;
+        }
       }
     }
-    if (remainder > 0)
-    {
-      printf("Nastąpiło przepełnienie!\n");
-      reset();
-      continue;
-    }
-    last_index = sizeof(result_reverse) / sizeof(result_reverse[0]) - 1;
-    for (int k = 0; k < MAX_LENGTH; k++)
-    {
-      sprintf(&result[k], "%d", result_reverse[last_index - k]);
-    }
-    for (num_start = 0; num_start < last_index; num_start++)
-    {
-      if (result[num_start] != '0')
-        break;
-    }
-    printf("Silnia %d wynosi: ", factorial);
-    for (; num_start < MAX_LENGTH; num_start++)
-    {
-      printf("%c", result[num_start]);
-    }
+    printf("Silnia z liczby %d wynosi: ", factorial);
+    
+    for (int i = current_idx; i >= 0; i--)
+      printf("%d", result_reverse[i]);
+    
+    free(result_reverse);
     printf("\n");
     reset();
   } while (1);
